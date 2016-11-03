0a66cdf274cddbff1deed539c075d81e41031773
Ben Carson <quod27@gmail.com>
2016-11-03 13:45:24 -0500

    Clean up

--- PG2BLC.java
+++ PG2BLC.java
@@ -10,8 +10,6 @@ import java.util.*;
 
 public class PG2BLC {
 
-    private static String newline = System.getProperty("line.separator");
-
     public static String readFile(String path) {
         try {
             byte[] encoded = Files.readAllBytes(Paths.get(path));
@@ -24,7 +22,7 @@ public class PG2BLC {
 
     public static boolean writeFile(String path, String content) {
         Path file = Paths.get(path);
-        List<String> lines = Arrays.asList(content.split(newline));
+        List<String> lines = Arrays.asList(content.split("\n"));
         try {
             Files.write(file, lines, Charset.forName("UTF-8"));
             return true;
@@ -35,26 +33,25 @@ public class PG2BLC {
     }
 
     public static String runLRUForFrames(String[] frameStrings, int frames) {
-        String out = "LRU algorithm with " + frames + " frames: " + newline + newline;
+        String out = "LRU algorithm with " + frames + " frames:\n";
         LRU lru = new LRU(frames);
         for (int i = 0; i < frameStrings.length; i++) {
             int page = Integer.parseInt(frameStrings[i]);
             lru.addPage(page);
-            out += lru + newline;
+            out += lru + "\n";
         }
-        out += newline + "Total number of page faults is " + lru.getPageFaults();
+        out += "\nTotal number of page faults is " + lru.getPageFaults();
         return out;
         }
 
     public static void main(String[] args) {
         String pageRef = readFile("pageRefString.txt");
         if (pageRef != null) {
-            String[] splited = pageRef.split("\\s+");
-            String out3Frames = runLRUForFrames(splited, 3);
-            String out4Frames = runLRUForFrames(splited, 4);
-            writeFile("pg2out.txt", out3Frames + newline + newline + out4Frames);
-            System.out.println(out3Frames);
-            System.out.println(out4Frames);
+            String[] splitted = pageRef.split("\\s+");
+            String out3Frames = runLRUForFrames(splitted, 3);
+            String out4Frames = runLRUForFrames(splitted, 4);
+            writeFile("pg2out.txt", out3Frames + "\n" + out4Frames);
+            System.out.println("Output written to pg2out.txt");
         }
     }
     
